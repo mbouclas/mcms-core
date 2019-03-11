99e4674e5a1ce16edbe68dea7cb4737fb079eff7
Michael Bouclas <mbouclas@gmail.com>
2019-03-11 22:50:01 +0200

    update to 5.7

--- src/Http/api.php
+++ src/Http/api.php
@@ -4,22 +4,3 @@ use Illuminate\Http\Request;
 
 
 Route::post('loginUser', '\App\Http\Controllers\Api\Auth@signIn');
-
-Route::get('me','\Mcms\Core\Http\Controllers\Api\Me@index')->middleware(['jwt.auth']);
-
-
-
-Route::post('register', '\Mcms\Core\Http\Controllers\JWTUserController@register');
-Route::post('login', '\Mcms\Core\Http\Controllers\JWTUserController@authenticate');
-Route::get('checkEmailNotTaken', '\Mcms\Core\Http\Controllers\JWTUserController@checkEmailNotTaken');
-
-Route::group(['middleware' => ['jwt.verify']], function() {
-    Route::get('user', '\Mcms\Core\Http\Controllers\JWTUserController@getAuthenticatedUser');
-    Route::get('me', '\Mcms\Core\Http\Controllers\Api\Me@index');
-    Route::get('boot', '\Mcms\Core\Http\Controllers\Api\Boot@index');
-    Route::get('refreshToken', '\Mcms\Core\Http\Controllers\JWTUserController@refreshToken');
-    Route::post('logout', '\Mcms\Core\Http\Controllers\JWTUserController@logout');
-    Route::get('/protectedTest', function (Request $request) {
-       return 'ok';
-    });
-});
