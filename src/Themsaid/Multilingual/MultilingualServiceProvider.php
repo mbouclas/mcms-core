b2ec986be81e345b7a839694ad15fd59001cea7a
mbouclas <mbouclas@gmail.com>
2022-04-10 20:57:03 +0300

    Upgrading to 6

---
+++ src/Themsaid/Multilingual/MultilingualServiceProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Themsaid\Multilingual;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Validator;
+
+class MultilingualServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $systemLocales = config('multilingual.locales');
+
+        $this->publishes([
+            __DIR__.'/config/multilingual.php' => config_path('multilingual.php'),
+        ]);
+
+
+        $this->app['validator']->extendImplicit('translatable_required', function ($attribute, $value, $parameters) use ($systemLocales) {
+            if (! is_array($value)) {
+                return false;
+            }
+
+            // Get only the locales that has a value and exists in the system locales array
+            $locales = array_filter(array_keys($value), function ($locale) use ($value, $systemLocales) {
+                return @$value[$locale] && in_array($locale, $systemLocales);
+            });
+
+            foreach ($systemLocales as $systemLocale) {
+                if (! in_array($systemLocale, $locales)) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/config/multilingual.php', 'multilingual'
+        );
+    }
+}
